81642a21455efe4be6ed5e5d977cda78036e1797
Andy Rustleund <andrew@rustleund.com>
2022-03-01 18:06:38 +1300

    Remove unneeded arguments

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,5 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 
 application {
     mainClass.set("rustleund.fightingfantasy.main.Main")
-    applicationDefaultJvmArgs = listOf("--add-opens java.base/java.lang=ALL-UNNAMED")
 }
 
