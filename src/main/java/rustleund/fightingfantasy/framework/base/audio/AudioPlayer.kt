25ecc4701b877251e7f50a203d15c9573f21f7ab
Andy Rustleund <andrew@rustleund.com>
2023-07-31 09:23:18 +1200

    Refactor player

--- src/main/java/rustleund/fightingfantasy/framework/base/audio/AudioPlayer.kt
+++ src/main/java/rustleund/fightingfantasy/framework/base/audio/AudioPlayer.kt
@@ -3,18 +3,19 @@ package rustleund.fightingfantasy.framework.base.audio
 import jaco.mp3.player.MP3Player
 import rustleund.fightingfantasy.framework.base.GameState
 import rustleund.fightingfantasy.framework.base.GameView
+import java.io.File
 
 
 class AudioPlayer : GameView {
 
-    private var player = MP3Player().setRepeat(false)
+    private var player = newPlayer()
 
     override fun update(gameState: GameState) {
         while (gameState.audioFilesToPlay.isNotEmpty()) {
             val audioFile = gameState.audioFilesToPlay.pop()
             if (audioFile.interrupt || !player.isPlaying) {
                 player.stop()
-                player = MP3Player(audioFile.path.toFile()).setRepeat(false)
+                player = newPlayer(audioFile)
                 player.play()
                 while (!player.isPlaying) {
                     Thread.sleep(5)
@@ -24,4 +25,7 @@ class AudioPlayer : GameView {
             }
         }
     }
+
+    private fun newPlayer(file: AudioFile? = null) =
+        (if (file == null) MP3Player() else MP3Player(file.path.toFile())).setRepeat(false).setVolume(75)
 }
